670717b408330537021407458382f0cd8c2b3ab7
ennaarid <paras.d.i.s.07@gmail.com>
2025-05-16 02:45:07 +0800

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -5,7 +5,17 @@ public interface UserService {
 
     void registerUser(String username, String password, String email);
 
+    void registerAdminUser(String username, String password, String email);
+
+    boolean updateUserRole(String username, String newRole);
+
     boolean resetPassword(String email, String token, String newPassword);
 
     boolean verifyResetToken(String token);
+
+    boolean changePassword(String username, String currentPassword, String newPassword);
+
+    boolean saveUserPreferences(String username, boolean emailNotifications, boolean smsNotifications, String theme);
+
+    Object[] loadUserPreferences(String username);
 }
